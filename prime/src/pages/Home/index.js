82d94f6169443225ec4f8d219c684d9b16bf8b0a
Pablo Sousa <pablo.ufopa@gmail.com>
2023-05-29 21:02:45 -0300

    Criando a Home

--- prime/src/pages/Home/index.js
+++ prime/src/pages/Home/index.js
@@ -1,5 +1,26 @@
+import { useEffect, useState} from 'react';
+import api from '../../services/api';
 
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+    
+    useEffect(()=>{
+
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key:"f2943cd0a8978a717cc6868e90938397",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+
+            console.log(response.data.results);
+        }
+
+        loadFilmes();
+
+    }, [])
     return(
         <div>
             <h1>BEM VINDO A HOME</h1>
