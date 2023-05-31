fa2d239ca3e35cbed8da1d8a70e8c0fdf626e647
Pablo Sousa <pablo.ufopa@gmail.com>
2023-05-31 19:40:09 -0300

    Listando Filmes

--- prime/src/pages/Home/index.js
+++ prime/src/pages/Home/index.js
@@ -1,31 +1,49 @@
 import { useEffect, useState} from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
+
+// URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
 
 function Home(){
-    const [filmes, setFilmes] = useState([]);
-    
-    useEffect(()=>{
+  const [filmes, setFilmes] = useState([]);
 
-        async function loadFilmes(){
-            const response = await api.get("movie/now_playing", {
-                params:{
-                    api_key:"f2943cd0a8978a717cc6868e90938397",
-                    language: "pt-BR",
-                    page: 1,
-                }
-            })
 
-            console.log(response.data.results);
+  useEffect(()=>{
+
+    async function loadFilmes(){
+      const response = await api.get("movie/now_playing", {
+        params:{
+         api_key: "28fc232cc001c31e8a031f419d0a14ca",
+         language: "pt-BR",
+         page: 1,
         }
+      })
 
-        loadFilmes();
+      //console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10))
 
-    }, [])
-    return(
-        <div>
-            <h1>BEM VINDO A HOME</h1>
-        </div>
-    )
+    }
+
+    loadFilmes();
+
+  }, [])
+
+  return(
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Home;
