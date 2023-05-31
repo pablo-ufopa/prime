ffb98468727b88103373f29807443e8999eb2b67
Pablo Sousa <pablo.ufopa@gmail.com>
2023-05-31 19:51:10 -0300

    Page not found

--- prime/src/routes.js
+++ prime/src/routes.js
@@ -3,18 +3,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 
+import Erro from './pages/Erro';
+
 import Header from './components/Header';
 
 function RoutesApp(){
-    return(
-        <BrowserRouter>
-            <Header/>
-            <Routes>
-                <Route path='/' element={<Home/>} />
-                <Route path='/filme/:id' element={<Filme/>} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return(
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="/filme/:id" element={ <Filme/> } />
+
+        <Route path="*" element={ <Erro/> } />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default RoutesApp;
